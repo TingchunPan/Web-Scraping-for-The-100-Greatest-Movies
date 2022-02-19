6287903d0b0e464ab076a9fbfe1345d4bd18d3c1
TingchunPan <87034968+TingchunPan@users.noreply.github.com>
2022-02-19 01:11:54 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+with open('movie_file.txt', 'w') as f:
+ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+ respond=requests.get(URL)
+ web=respond.text
+ soup=BeautifulSoup(web,"html.parser")
+ articles = soup.find_all(name="h3", class_="title")
+ rankings=[]
+ for rank in articles:
+     article_ranking=rank.getText()
+     rankings.append(article_ranking+"\n")
+
+ f.writelines(reversed(rankings))
+ f.close()
